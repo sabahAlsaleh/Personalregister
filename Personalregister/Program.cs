18dfb49b3c1df71ed841732e5e017371f975ad53
Sabah <sabahaa@kth.se>
2024-09-10 15:02:54 +0200

    Add project files.

---
+++ Personalregister/Program.cs
@@ -0,0 +1,76 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Welcome to Employee Register! ");
+EmployeeRegister register = new EmployeeRegister();
+
+
+while (true)
+{
+    Console.WriteLine(" Enter Employee name OR  q  to quti : ");
+
+    string name = Console.ReadLine();
+    if (name.ToLower() == "q") break;
+    Console.WriteLine("  Enter  Employee salary : ");
+    if (decimal.TryParse(Console.ReadLine(), out decimal salary))
+    {
+        Employee employee = new Employee(name, salary);
+        register.addEmployee(employee);
+    }
+    else
+    {
+        Console.WriteLine("Invalid salary input. Please try again.");
+    }
+
+
+}
+register.DisplayRegister();
+
+
+
+
+public class Employee
+{
+    public string Name { get; set; }
+    public decimal Salary { get; set; }
+
+    public Employee(string name, decimal salary)
+    {
+        Name = name;
+        Salary = salary;
+    }
+
+    public void ShowEmployeeInfo()
+    {
+        Console.WriteLine($"Name: {Name} , Salary: {Salary}  Kr");
+    }
+
+}
+
+public class EmployeeRegister
+{
+    private List<Employee> employees = new List<Employee>();
+
+    public void addEmployee(Employee employee)
+    {
+        employees.Add (employee);
+    }
+
+
+    public void DisplayRegister()
+    {
+        Console.WriteLine("Employee Register:");
+
+        foreach (Employee emp in employees)
+        {
+            emp.ShowEmployeeInfo();      
+        }
+
+
+    }
+
+
+}
+
+
+
+
